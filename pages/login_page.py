388c4f084e7e8702d48720b658dc5ea314076a3d
ddrkt <toxa.ship@gmail.com>
2020-04-14 05:12:15 +0300

    'is it login page?' added to test_guest_can_go_to_login_page

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,7 +1,9 @@
 from .base_page import BasePage
 from .locators import LoginPageLocators
 
+
 class LoginPage(BasePage):
+
     def should_be_login_page(self):
         self.should_be_login_url()
         self.should_be_login_form()
